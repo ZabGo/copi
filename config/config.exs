b1c3e0a80829e77c5633ce412a63183ff2ec0cef
ZabGo <xavgodard@gmail.com>
2024-05-14 12:36:17 +0100

    setup gettext for French translation

--- config/config.exs
+++ config/config.exs
@@ -48,3 +48,5 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :copi, CopiWeb.Gettext, default_locale: "fr", locales: ~w(en fr)
